9556d22d25abac34f085a9bb0fd1c8bd481838a0
sky0014 <sky-wang@qq.com>
2023-06-27 17:08:33 +0800

    update

--- src/app/component/todo/index.tsx
+++ src/app/component/todo/index.tsx
@@ -1,13 +1,26 @@
+import { Checkbox } from "antd";
 import React, { memo } from "react";
 import { TodoItem, todo } from "src/store/todo";
+
+import style from "./index.module.styl";
 
 function Todo(props: { todo: TodoItem }) {
   console.log("render todo");
 
   return (
-    <div onClick={() => todo.completeTodo(props.todo.id)}>
-      {props.todo.content} {props.todo.isDone && "done"}
-    </div>
+    <Checkbox
+      className={props.todo.isDone && style.done}
+      defaultChecked={props.todo.isDone}
+      onChange={(event) => {
+        if (event.target.checked) {
+          todo.completeTodo(props.todo.id);
+        } else {
+          todo.unCompleteTodo(props.todo.id);
+        }
+      }}
+    >
+      {props.todo.content}
+    </Checkbox>
   );
 }
 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./index.styl";
 import Catalog from "src/app/component/catalog";
 import { catalog } from "src/store/catalog";
+import { Collapse, ConfigProvider, Input, InputRef, Modal } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { todo } from "src/store/todo";
 
 function App() {
   console.log("render app");
 
+  const addInputRef = useRef<InputRef>(null);
+
   return (
-    <div>
-      {Object.values(catalog.items).map((catalog) => (
-        <Catalog key={catalog.id} catalog={catalog} />
-      ))}
-    </div>
+    <ConfigProvider
+      theme={{
+        components: {
+          Checkbox: {
+            colorPrimary: "#d9d9d9",
+            colorPrimaryHover: "#8c8c8c",
+          },
+        },
+      }}
+    >
+      <Collapse
+        defaultActiveKey={Object.values(catalog.items).map(
+          (catalog) => catalog.id
+        )}
+        items={Object.values(catalog.items).map((catalog) => ({
+          key: catalog.id,
+          label: `${catalog.description} (${catalog.name})`,
+          children: <Catalog key={catalog.id} catalog={catalog} />,
+          extra: (
+            <PlusOutlined
+              onClick={(event) => {
+                event.stopPropagation();
+                Modal.confirm({
+                  icon: null,
+                  title: "请输入",
+                  content: (
+                    <Input placeholder="您的待办事项" ref={addInputRef} />
+                  ),
+                  onOk() {
+                    const content = addInputRef.current?.input?.value;
+                    if (content) {
+                      todo.addTodo({
+                        catalogId: catalog.id,
+                        content,
+                      });
+                    }
+                  },
+                });
+              }}
+            />
+          ),
+        }))}
+      />
+    </ConfigProvider>
   );
 }
 
